bbdb0196148f7712b112dbd60e741f190da05e72
Nguyễn Trung Hiếu <trunghieu99tt@gmail.com>
2020-05-27 14:43:40 +0700

    Updates

---
+++ precache-manifest.ac1d4dcadb9c4e97d65a7adc336462ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b80016f75081eb4c069dc604e541ed",
+    "url": "/avante/index.html"
+  },
+  {
+    "revision": "c026f622d9aa58ee724b",
+    "url": "/avante/static/css/2.a771ffcd.chunk.css"
+  },
+  {
+    "revision": "4812391973261b3c001c",
+    "url": "/avante/static/css/main.60ee2475.chunk.css"
+  },
+  {
+    "revision": "c026f622d9aa58ee724b",
+    "url": "/avante/static/js/2.f36db327.chunk.js"
+  },
+  {
+    "revision": "07826c8b8180555768ebcc32029a3c2e",
+    "url": "/avante/static/js/2.f36db327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4812391973261b3c001c",
+    "url": "/avante/static/js/main.d59f1e1f.chunk.js"
+  },
+  {
+    "revision": "43dfb6cfa74312a6032b",
+    "url": "/avante/static/js/runtime-main.913eec4e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/avante/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/avante/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/avante/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/avante/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/avante/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
